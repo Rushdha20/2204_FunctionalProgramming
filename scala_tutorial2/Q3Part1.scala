314ab7b581c46dc058f102153c07d1f5d7c3c046
Rushdha20 <89083702+Rushdha20@users.noreply.github.com>
2022-07-10 12:10:20 +0530

    Add files via upload

---
+++ scala_tutorial2/Q3Part1.scala
@@ -0,0 +1,26 @@
+object Q3Part1 extends App{
+  var k,i,j=2
+  var m,n=5
+  var f=12.0f
+  var g=4.0f
+  var c='X'
+
+  //a)
+  k+12*m
+
+  //b)
+  m/j
+
+  //c)
+  n%j
+
+  //d)
+  m/j*j
+
+  //e)
+  f+10*5+g
+
+  //f)
+  i+=1
+  i*n
+}
