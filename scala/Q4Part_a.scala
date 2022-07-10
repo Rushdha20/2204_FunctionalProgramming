a6138b137446fb455d0ea878b3ce84a77b76619d
Rushdha20 <89083702+Rushdha20@users.noreply.github.com>
2022-07-10 12:12:36 +0530

    Delete scala directory

--- scala/Q4Part_a.scala
+++
@@ -1,8 +0,0 @@
-object Q4Part_a extends App{
-  def wage(workhr:Int):Int=workhr*250
-  def ot(OThr:Int):Int=OThr*85
-  def income(workhr:Int,OThr:Int):Int=wage(workhr)+ot(OThr)
-  def tax(income:Int):Double=income*.12
-  def takeHomeSalary(workhr:Int,OThr:Int):Double=income(workhr,OThr)-tax(income(workhr,OThr))
-  println("Rs. "+takeHomeSalary(40,30))
-}
